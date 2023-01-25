885352d489fe8533f664bcb94a3aa2fac98bda49
SAPIENS\Amey.Panade <Amey.Panade@sapiens.com>
2023-01-25 18:45:12 +0530

    App service

---
+++ Test_importData_delete/functions/getContact.js
@@ -0,0 +1,11 @@
+// This function is the endpoint's request handler.
+exports = async function({ query, headers, body}, response) {
+    const {contactId} = query;
+    const collectionContact = context.services.get('mongodb-atlas').db('Digital').collection('contact');
+    
+    
+    return await collectionContact.findOne({"contactId":contactId})
+      .then(data => {
+        return data;
+  });
+};
